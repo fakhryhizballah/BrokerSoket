b75d259f62a53839b1e2491a6a1ef3f389b27f9e
fakhryhizballah <falehry88@gmail.com>
2022-06-24 17:34:52 +0700

    socket deploy

--- socket.js
+++ socket.js
@@ -1,9 +1,36 @@
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const io = new Server({ /* options */ });
+const httpServer = createServer();
+const port = 3800;
+const io = new Server(httpServer, {
+    /* options */
+    cors: {
+        origin: "http://127.0.0.1:5500/",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"]
+    },
 
+});
+var user = 0;
 io.on("connection", (socket) => {
-  // ...
+    console.log(socket.id);
+    user++;
+    console.log("user online : " + user);
+    io.emit("pesan", "user online " + user);
+
+    socket.on("disconnect", (reason) => {
+        user--;
+        console.log(reason)
+        console.log("user online : " + user);
+        io.emit("pesan", "user online " + user);
+    });
+
 });
 
-io.listen(3000);
+httpServer.listen(port, function () {
+    console.log("socket.io run port : " + port)
+});
+module.exports = {
+    io
+}
