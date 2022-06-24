16ad863da0d40af1b630b918610ddb460ab5f894
fakhryhizballah <falehry88@gmail.com>
2022-06-24 17:52:58 +0700

    remove origin server

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const port = 3800;
 const io = new Server(httpServer, {
     /* options */
     cors: {
-        origin: "http://127.0.0.1:5500/",
+        // origin: "http://127.0.0.1:5500/",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"]
     },
